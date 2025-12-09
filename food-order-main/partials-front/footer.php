61f348af04e189efe255a3a76be97b8f12f99ef9
Gaurav4633 <gourav4633@gmail.com>
2025-12-09 16:48:08 +0530

    added project files

---
+++ food-order-main/partials-front/footer.php
@@ -0,0 +1,35 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                 <li>
+                 <a href="https://www.linkedin.com/in/gaurav-kumar-984978376/"><img src="https://img.icons8.com/ios-filled/50/0A66C2/linkedin.png" ></a>
+                </li>
+
+                 <li>
+                  <a href="#"> <img src="https://img.icons8.com/ios-filled/50/000000/github.png" /></a>
+
+                </li>
+               
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+               
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed inside <a href="#">GAURAV</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
